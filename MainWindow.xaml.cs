321aa7ce901b219d5138459575a34ec680f3f0ea
Jverhagen1993 <60172900+Jverhagen1993@users.noreply.github.com>
2020-06-07 19:16:44 +0200

    Update Final

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drankregistratie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgDranken.ItemsSource = db.Drankens.ToList();
+        }
+
+        private void dgDranken_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var drank = (Dranken)dgDranken.SelectedItem;
+            txtNaam.Text = drank.NAAM;
+            txtSoort.Text = drank.SOORT;
+            txtPrijs.Text = drank.PRIJS;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var drank = new Dranken();
+            drank.NAAM = txtNaam.Text;
+            drank.SOORT = txtSoort.Text;
+            drank.PRIJS = txtPrijs.Text;
+            db.Drankens.InsertOnSubmit(drank);
+            db.SubmitChanges();
+
+            dgDranken.ItemsSource = db.Drankens.ToList();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            dgMain.Children.Clear();
+            dgMain.Children.Add(new UserControl1());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNaam.Text != null)
+            {
+                var DrUpdate = (Dranken)dgDranken.SelectedItem;
+                DrUpdate.NAAM = txtNaam.Text;
+                DrUpdate.SOORT = txtSoort.Text;
+                DrUpdate.PRIJS = txtPrijs.Text;
+                db.SubmitChanges();
+
+                dgDranken.ItemsSource = db.Drankens.ToList();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                DataClasses1DataContext dr = new DataClasses1DataContext();
+                Dranken DrDelete = dgDranken.SelectedItem as Dranken;
+                var drankie = (from Dranken in dr.Drankens
+                               where Dranken.ID == DrDelete.ID
+                               select Dranken).Single();
+                dr.Drankens.DeleteOnSubmit(drankie);
+                dr.SubmitChanges();
+                dgDranken.ItemsSource = db.Drankens.ToList();
+
+            }
+        }
+    }
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drankregistratie
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnID_Click(object sender, RoutedEventArgs e)
+        {
+            string ID = txtID.Text;
+            var show = db.Drankens.Where(p => Convert.ToString(p.ID).Contains(ID));
+            dgDrank.ItemsSource = show;
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNaam.Text != null)
+            {
+                var DrUpdate = (Dranken)dgDrank.SelectedItem;
+                DrUpdate.NAAM = txtNaam.Text;
+                DrUpdate.SOORT = txtSoort.Text;
+                DrUpdate.PRIJS = txtPrijs.Text;
+                db.SubmitChanges();
+
+                string ID = txtID.Text;
+                var show = db.Drankens.Where(p => Convert.ToString(p.ID).Contains(ID));
+                dgDrank.ItemsSource = show;
+            }
+        }
+
+        private void dgDrank_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var drank = (Dranken)dgDrank.SelectedItem;
+            txtNaam.Text = drank.NAAM;
+            txtSoort.Text = drank.SOORT;
+            txtPrijs.Text = drank.PRIJS;
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+     
+            {
+                DataClasses1DataContext dr = new DataClasses1DataContext();
+                Dranken DrDelete = dgDrank.SelectedItem as Dranken;
+                var drankie = (from Dranken in dr.Drankens
+                        where Dranken.ID == DrDelete.ID
+                        select Dranken).Single();
+                dr.Drankens.DeleteOnSubmit(drankie);
+                dr.SubmitChanges();
+                dgDrank.ItemsSource = db.Drankens.ToList();
+
+                string ID = txtID.Text;
+                var show = db.Drankens.Where(p => Convert.ToString(p.ID).Contains(ID));
+                dgDrank.ItemsSource = show;
+            }
+        }
+    }
+}
